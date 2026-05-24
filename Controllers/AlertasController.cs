3d2d34a3fa56d682de318f83489440f42fde100f
Ramolo231 <23506540@ugaro.mx>
2026-05-23 20:10:18 -0600

    Corrigiendo AlertasController

--- Controllers/AlertasController.cs
+++ Controllers/AlertasController.cs
@@ -3,34 +3,22 @@ using NanoGuardian.Api.Models;
 
 namespace NanoGuardian.Api.Controllers
 {
+   
     [ApiController]
     [Route("api/[controller]")]
     public class AlertasController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult RecibirAlerta([FromBody] Alerta nuevaAlerta)
+        [HttpGet("ultima")]
+        public IActionResult ObtenerUltimaAlerta()
         {
-            if (string.IsNullOrEmpty(nuevaAlerta.Paciente))
+            var alerta = new Alerta
             {
-                return BadRequest("El nombre del paciente es obligatorio.");
-            }
+                Paciente = "Juan Pérez (Real)",
+                FuerzaImpactoG = 5,
+                Estado = "¡Caída Detectada en API!"
+            };
 
-            Console.WriteLine($"🚨 ALERTA RECIBIDA: Paciente {nuevaAlerta.Paciente} cayó con {nuevaAlerta.FuerzaImpactoG}G");
-
-            return Ok(new { mensaje = "Alerta procesada con éxito", codigo = 200 });
+            return Ok(alerta);
         }
     }
-    
-    [HttpGet("ultima")]
-public IActionResult ObtenerUltimaAlerta()
-{
-    var alerta = new Alerta
-    {
-        Paciente = "Juan Pérez (Real)",
-        FuerzaImpactoG = 5,
-        Estado = "¡Caída Detectada en API!"
-    };
-
-    return Ok(alerta);
-}
 }
